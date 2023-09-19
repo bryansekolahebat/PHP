93303495a8f08f260edeab84dbd9be2a6a787e9c
sengciu <141599133+bryansekolahebat@users.noreply.github.com>
2023-09-19 13:44:30 +0700

    Add files via upload

---
+++ add_user.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- check -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Tambah Pengguna</title>
+</head>
+<body>
+	<form action="koneksi.php" method="POST">
+		<input type="text" name="username" placeholder="Username" required>
+		<input type="text" name="name" placeholder="Name" required>
+		<input type="email" name="email" placeholder="Email" required>
+		<input type="password" name="password" placeholder="Password" required>
+		<input type="submit" name="submit">
+	</form>
+</body>
+</html>
